9355d355bd4dee7c14becae13992d82b71ed6076
Alex <82205868+fila109@users.noreply.github.com>
2023-09-08 19:33:16 +0300

    added files

---
+++ src/components/BaseFormField.vue
@@ -0,0 +1,12 @@
+<template>
+  <label class="form__label">
+    <slot></slot>
+    <span class="error" style="color: red;" v-if="error">{{ error }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: ['title', 'error'],
+};
+</script>
---
+++ src/components/BaseFormText.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseFormField :title=title :error=error>
+    <input class="form__input" v-model="dataValue" :placeholder=placeholder :type=type>
+  </BaseFormField>
+</template>
+
+<script>
+
+import formFieldMixin from '@/mixins/formFieldMixin.js';
+
+export default {
+  props: ['type'],
+  mixins: [formFieldMixin],
+};
+</script>
